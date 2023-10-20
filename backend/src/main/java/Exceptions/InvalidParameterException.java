94bbccdd1540abf98ae083ebfed71223624ee441
Alice Nguyen <90719648+aintic@users.noreply.github.com>
2023-10-20 00:11:12 -0400

    remove duplicates

--- backend/src/main/java/Exceptions/InvalidParameterException.java
+++
@@ -1,7 +0,0 @@
-package src.main.java.Exceptions;
-
-public class InvalidParameterException extends Exception {
-    public InvalidParameterException(String message) {
-        super(message);
-    }
-}
