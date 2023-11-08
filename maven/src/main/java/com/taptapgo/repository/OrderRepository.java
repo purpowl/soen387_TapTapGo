784645b23df1e079b6b7e43625fed2122c37e128
Whitewolf04 <tominhtuan18@gmail.com>
2023-11-08 11:44:07 -0500

    Finished create and read operations for OrderRepo

--- maven/src/main/java/com/taptapgo/repository/OrderRepository.java
+++ maven/src/main/java/com/taptapgo/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Savepoint;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.sql.Connection;
@@ -92,21 +93,28 @@ public class OrderRepository{
         }
     }
 
-    public static Order readOrderID(int orderID) {
-        String getOrderQuery = "SELECT OrderPayDate, TotalAmt, PayMethod, 4CreditDigits, BillAddress, BillCity, BillCountry, BillPostalCode, ShippingStatus, TrackingNumber, ShipDate, ShipAddress, ShipCity, ShipCountry, ShipPostalCode, CustomerID, GCID FROM order WHERE OrderID = ?";
-        String getOrderItemsQuery = "SELECT ProductSKU, Quantity FROM order WHERE OrderID = ?";
+
+    /**
+     * Get an order given the order ID
+     * @param orderID the order ID that is being searched for
+     * @return An Order object, this will be null if no order is found with the corresponding ID
+     */
+    public static Order readOrderByID(int orderID) {
+        String getOrderQuery = "SELECT OrderPayDate, TotalAmt, PayMethod, 4CreditDigits, BillAddress, BillCity, BillCountry, BillPostalCode, ShippingStatus, TrackingNumber, ShipDate, ShipAddress, ShipCity, ShipCountry, ShipPostalCode, CustomerID, GCID FROM `order` WHERE OrderID = ?";
+        String getOrderItemsQuery = "SELECT ProductSKU, Quantity FROM `order` WHERE OrderID = ?";
         
         try {
+            // Open database connection
             Class.forName("com.mysql.jdbc.Driver");
-
             db_conn = DriverManager.getConnection("jdbc:mysql://taptapgo.mysql.database.azure.com:3306/taptapgo?characterEncoding=UTF-8", "soen387_taptapgo", "T@pT@pG0387");
 
+            // Get an order that has this orderID
             PreparedStatement pstmt = db_conn.prepareStatement(getOrderQuery);
             pstmt.setInt(1, orderID);
-
             ResultSet queryResult = pstmt.executeQuery();
 
             if (queryResult.next()) {
+                // If an order is found, extract all order information
                 Date payDate = queryResult.getDate(1);
                 float totalAmount = queryResult.getFloat(2);
                 String payMethod = queryResult.getString(3);
@@ -125,13 +133,17 @@ public class OrderRepository{
                 String customerID = queryResult.getString(16);
                 String guestCustomerID = queryResult.getString(17);
                 Customer customer = null;
+                queryResult.close();
 
+                // TODO: Implement CustomerIdentityMap before testing this
                 if (customerID != null) {
                     customer = CustomerIdentityMap.getRegisteredCustomer(customerID);
                 } else {
                     customer = CustomerIdentityMap.getGuestCustomer(guestCustomerID);
                 }
 
+
+                // Extract all items that are in this order
                 HashMap<Product, Integer> orderItems = new HashMap<Product, Integer>();
                 PreparedStatement pstmt2 = db_conn.prepareStatement(getOrderItemsQuery);
                 pstmt2.setInt(1, orderID);
@@ -143,15 +155,131 @@ public class OrderRepository{
                     Product product = Warehouse.getInstance().findProductBySKU(productSKU);
                     orderItems.put(product, quantity);
                 }
+                queryResult.close();
+
+                db_conn.close();
 
                 return Order.loadOrder(orderID, totalAmount, billAddress, billCity, billCountry, billPostalCode, payMethod, cardNum, payDate, shipAddress, shipCity, shipCountry, shipPostalCode, shipStatus, trackingNum, shipDate, customer, orderItems);
             } else {
+                // If order is not found, return null
                 return null;
             }
 
         } catch(Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+
+    /**
+     * Get all matching orders from the database given a customer object
+     * @param customer the customer that we are search orders for
+     * @param ordersLoaded A Hashmap of all orders that are already loaded in memory, so we avoid reloading these orders
+     * @return A Hashmap mapping orderID to an Order object. This hashmap will be empty if no order is found.
+     */
+    public static HashMap<Integer, Order> readOrderByCustomer(Customer customer, HashMap<Integer, Order> ordersLoaded) {
+        String getOrderQuery = "SELECT OrderID, OrderPayDate, TotalAmt, PayMethod, 4CreditDigits, BillAddress, BillCity, BillCountry, BillPostalCode, ShippingStatus, TrackingNumber, ShipDate, ShipAddress, ShipCity, ShipCountry, ShipPostalCode FROM `order` WHERE CustomerID = ?";
+        String getOrderItemsQuery = "SELECT ProductSKU, Quantity FROM `order` WHERE OrderID = ?";
+        HashMap<Integer, Order> orderResults = new HashMap<Integer, Order>();
+
+        try {
+            // Open database connection
+            Class.forName("com.mysql.jdbc.Driver");
+            db_conn = DriverManager.getConnection("jdbc:mysql://taptapgo.mysql.database.azure.com:3306/taptapgo?characterEncoding=UTF-8", "soen387_taptapgo", "T@pT@pG0387");
+
+            // Query the database to get all orders of this customer
+            PreparedStatement pstmt = db_conn.prepareStatement(getOrderQuery);
+            pstmt.setString(1, customer.getUserID());
+            ResultSet queryResult = pstmt.executeQuery();
+
+            while (queryResult.next()) {
+                // Extract all order information
+                int orderID = queryResult.getInt(1);
+                // If order is already loaded, skip
+                if(ordersLoaded.get(orderID) != null) {
+                    continue;
+                }
+                Date payDate = queryResult.getDate(2);
+                float totalAmount = queryResult.getFloat(3);
+                String payMethod = queryResult.getString(4);
+                int cardNum = queryResult.getInt(5);
+                String billAddress = queryResult.getString(6);
+                String billCity = queryResult.getString(7);
+                String billCountry = queryResult.getString(8);
+                String billPostalCode = queryResult.getString(9);
+                String shipStatus = queryResult.getString(10);
+                String trackingNum = queryResult.getString(11);
+                Date shipDate = queryResult.getDate(12);
+                String shipAddress = queryResult.getString(13);
+                String shipCity = queryResult.getString(14);
+                String shipCountry = queryResult.getString(15);
+                String shipPostalCode = queryResult.getString(16);
+                queryResult.close();
+
+
+                // Extract all order items within this order
+                HashMap<Product, Integer> orderItems = new HashMap<Product, Integer>();
+                PreparedStatement pstmt2 = db_conn.prepareStatement(getOrderItemsQuery);
+                pstmt2.setInt(1, orderID);
+                queryResult = pstmt2.executeQuery();
+
+                while (queryResult.next()) {
+                    String productSKU = queryResult.getString(1);
+                    int quantity = queryResult.getInt(2);
+                    Product product = Warehouse.getInstance().findProductBySKU(productSKU);
+                    orderItems.put(product, quantity);
+                }
+                queryResult.close();
+
+                
+                // Create an order and load to result hashmap
+                Order newOrder = Order.loadOrder(orderID, totalAmount, billAddress, billCity, billCountry, billPostalCode, payMethod, cardNum, payDate, shipAddress, shipCity, shipCountry, shipPostalCode, shipStatus, trackingNum, shipDate, customer, orderItems);
+                orderResults.put(newOrder.getOrderID(), newOrder);
+
+            }
+
+            db_conn.close();
+
+            return orderResults;
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return orderResults;
+        }
+    }
+
+
+    /**
+     * Get the maximum order ID from the order
+     * This function is called at server start-up to update the order counter
+     * @return the maximum order ID currently present in database
+     */
+    public static int getMaxOrderID() {
+        String getMaxOrderIDQuery = "SELECT MAX(OrderID) FROM `order`";
+        int maxOrderID = 0;
+
+        try {
+            // Open database connection
+            Class.forName("com.mysql.jdbc.Driver");
+            db_conn = DriverManager.getConnection("jdbc:mysql://taptapgo.mysql.database.azure.com:3306/taptapgo?characterEncoding=UTF-8", "soen387_taptapgo", "T@pT@pG0387");
+
+            // Query the database to get the maximum orderID
+            Statement stmt = db_conn.createStatement();
+            ResultSet result = stmt.executeQuery(getMaxOrderIDQuery);
+
+            if (result.next()) {
+                maxOrderID = result.getInt(1);
+            }
+
+            stmt.close();
+            db_conn.close();
+
+            return maxOrderID;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return maxOrderID;
         }
     }
 
