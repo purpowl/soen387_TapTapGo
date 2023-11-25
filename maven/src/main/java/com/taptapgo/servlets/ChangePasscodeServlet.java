7929302b017e21ff78071d038b3f32fcda081b66
Alice Nguyen <90719648+aintic@users.noreply.github.com>
2023-11-25 13:00:47 -0500

    implement change passcode

---
+++ maven/src/main/java/com/taptapgo/servlets/ChangePasscodeServlet.java
@@ -0,0 +1,57 @@
+package com.taptapgo.servlets;
+
+import java.io.IOException;
+import com.taptapgo.User;
+import com.taptapgo.exceptions.InvalidParameterException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import com.taptapgo.repository.UserIdentityMap;
+import org.apache.commons.lang3.StringUtils;
+
+@WebServlet(name = "changePasscodeServlet", value = "/change-passcode")
+public class ChangePasscodeServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+        // get request parameters for passcode
+        String oldpwd = request.getParameter("oldpwd");
+        String newpwd = request.getParameter("newpwd");
+
+        if ((newpwd.length() < 4) || !(StringUtils.isAlphanumeric(newpwd))) {
+            // output fail message
+            response.sendRedirect(request.getContextPath() + "/modify-pwd.jsp?modify=fail");
+            return;
+        }
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("registered_user");
+
+        // authenticate old password
+        if (user.getUserID().equals(UserIdentityMap.authenticate(oldpwd))) {
+            try {
+                // try to change passcode
+                boolean passcodeChanged = user.setPasscode(newpwd);
+
+                if (passcodeChanged) {
+                    // output success message
+                    response.sendRedirect(request.getContextPath() + "/modify-pwd.jsp?modify=success");
+                }
+                else {
+                    // output fail message - new passcode is invalid
+                    response.sendRedirect(request.getContextPath() + "/modify-pwd.jsp?modify=fail");
+                }
+            }
+            catch (InvalidParameterException e) {
+                e.printStackTrace();
+                // output fail message
+                response.sendRedirect(request.getContextPath() + "/modify-pwd.jsp?modify=fail");
+            }
+        }
+        else {
+            // output fail message
+            response.sendRedirect(request.getContextPath() + "/modify-pwd.jsp?modify=fail");
+        }
+    }
+}
